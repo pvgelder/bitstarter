5dd90e257fa372dad77a8a261af77e10284038d0
pvgelder <paul.vangelder@xs4all.nl>
2015-07-04 10:44:36 +0000

    Fixed index.js to use buffers properly

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express')
 var app = express();
 
+var fs = require('fs');
+var buf = new Buffer(fs.readFileSync('index.html');
+var message = buf.toString();
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello World2!')
+  responde.send(message);
 })
 
 app.listen(app.get('port'), function() {
