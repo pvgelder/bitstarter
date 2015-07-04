f20f7b816e41b67a1cb79a834a6ec3bf957d75cb
pvgelder <paul.vangelder@xs4all.nl>
2015-07-04 10:58:54 +0000

    test

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var express = require('express')
 var app = express();
 
 var fs = require('fs');
-var buf = new Buffer(fs.readFileSync('index.html');
-//var buf = fs.readFileSync('index.html');
+//var buf = new Buffer(fs.readFileSync('index.html');
+var buf = fs.readFileSync('index.html');
 var message = buf.toString();
 
 app.set('port', (process.env.PORT || 5000))
@@ -11,7 +11,8 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response, message) {
   response.send('Hello World2!')
-  responde.send(message);
+  response.send(message);
+  response.send('done?');
 })
 
 app.listen(app.get('port'), function() {
